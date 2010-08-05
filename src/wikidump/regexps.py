80aac9fac282b2b1eb28dd808406f0c904ea8d8f
Marco Lui <saffsd@gmail.com>
2010-08-05 11:59:57 +1000

    Added a new regexp for matching category link.

--- src/wikidump/regexps.py
+++ src/wikidump/regexps.py
@@ -13,4 +13,5 @@ lang_prefixes = '|'.join(lang.prefixes)
 lang_link = re.compile(r"\[\[(?P<prefix>"+lang_prefixes+"):(?P<title>.+?)\]\]")
 
 category_keywords = '|'.join(lang.category_identifier.values())
-category_link = re.compile(r"\[\[(?P<title>("+category_keywords+"):.+?)\|.+?\]\]")
+category_link = re.compile(r"\[\[(?P<title>("+category_keywords+"):(?P<category>.+?))\|.+?\]\]")
+category_name = re.compile(r"(?P<keyword>("+category_keywords+")):(?P<category>.+)")
