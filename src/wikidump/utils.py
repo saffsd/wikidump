be4ece8da698b6711ebcc4bed00649dd97f2ebc6
Marco Lui <saffsd@gmail.com>
2010-08-06 13:44:25 +1000

    Removed sampling code that is not actually used.

--- src/wikidump/utils.py
+++ src/wikidump/utils.py
@@ -1,8 +1,6 @@
 import os
 import re
-import random
 import logging
-import numpy
 
 from collections import defaultdict
 
@@ -20,34 +18,11 @@ all_prefixes =  [    regexps.dumpfile_name.match(f).group(1)
                 if   regexps.dumpfile_name.match(f)
                 ]
 
-def weighted_choice(items, weights=None, count=1, seed=83441363):
-  numpy.random.seed(seed)
-  if hasattr(items, 'values'): 
-    items, weights = zip(*items.items())
-   
-  if not weights: weights = len(items) * [1]
-
-  # Normalize to 1 if required
-  t = float(sum(weights))
-
-  # Uniform chance if weights sum to 0
-  if t == 0.0: weights = [ 1/len(items) for i in items ]
-  elif t != 1.0: weights = [ w/t for w in weights ]
-
-  dist = numpy.cumsum(weights)
-  result = numpy.searchsorted(dist, numpy.random.random(count))
-
-  if count == 1:
-    return list(numpy.array(items)[result])[0]
-  else:
-    return list(numpy.array(items)[result])
-
-def ceil_half(x):
-  return x[:int(math.ceil(len(x) / 2.0))]
-      
 def load_dumps(langs=None, dump_path=None, build_index=False):
   "Load the dumps, and take note of their size"
   # Take note that we will end up loading all the dumps we have for a given language
+  # but only returning the last one we find. 
+  # TODO: Handle different-dated dumps of the same language
   if langs is None:
     langs = all_prefixes
   if dump_path is None:
