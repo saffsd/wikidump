312fad411bffb93180650ebb2f3c7982884b4935
Marco Lui <saffsd@gmail.com>
2011-02-01 10:26:57 +1100

    extended list of language prefixes

--- src/wikidump/lang.py
+++ src/wikidump/lang.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 # Static constants for dealing with different langauge wikis
 
-# List of wikipedia prefixes - not exhaustive
-prefixes = ['ru', 'nap', 'ja', 'ta', 'tl', 'az', 'ht', 'et', 'ca', 'wa', 'pms', 
-'de', 'nl', 'su', 'bpy', 'fa', 'tr', 'en', 'eu', 'io', 'jv', 'nds', 'pt', 'zh', 
-'bn', 'lt', 'la', 'nn', 'no', 'he', 'sh', 'lb', 'ro', 'hr', 'scn', 'da', 'it', 
-'te', 'br', 'an', 'fr', 'cs', 'is', 'new', 'lv', 'ast', 'uk', 'mk', 'oc', 'ku', 
-'sl', 'sk', 'af', 'ka', 'pl', 'ceb', 'th', 'sv', 'id', 'bs', 'hi', 'el', 'cy', 
-'gl', 'vi', 'sq', 'fi', 'mr', 'hu', 'ar', 'bg', 'ms', 'ko', 'be']
+# List of wikipedia prefixes with > 1000 documents at August 2010
+prefixes = [ 
+  'aa', 'ab', 'af', 'ak', 'als', 'am', 'an', 'ang', 'ar', 'arc', 'arz', 'as', 
+  'ast', 'av', 'ay', 'az', 'ba', 'bar', 'bcl', 'be', 'bg', 'bh', 'bi', 'bm',
+  'bn', 'bo', 'bpy', 'br', 'bs', 'bug', 'bxr', 'ca', 'cdo', 'ce', 'ceb', 'ch',
+  'cho', 'chr', 'chy', 'co', 'cr', 'crh', 'cs', 'csb', 'cu', 'cv', 'cy', 'da',
+  'de', 'diq', 'dsb', 'dv', 'dz', 'ee', 'el', 'eml', 'en', 'eo', 'es', 'et',
+  'eu', 'ext', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'frp', 'fur', 'fy', 'ga',
+  'gan', 'gd', 'gl', 'glk', 'gn', 'got', 'gu', 'gv', 'ha', 'hak', 'haw', 'he',
+  'hi', 'hif', 'ho', 'hr', 'hsb', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie',
+  'ig', 'ii', 'ik', 'ilo', 'io', 'is', 'it', 'iu', 'ja', 'jbo', 'jv', 'ka',
+  'kaa', 'kab', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks',
+  'ksh', 'ku', 'kv', 'kw', 'ky', 'la', 'lad', 'lb', 'lbe', 'lg', 'li', 'lij',
+  'lmo', 'ln', 'lo', 'lt', 'lv', 'mdf', 'mg', 'mh', 'mhr', 'mi', 'mk', 'ml',
+  'mn', 'mo', 'mr', 'ms', 'mt', 'mus', 'my', 'myv', 'mzn', 'na', 'nah', 'nap',
+  'nds', 'ne', 'new', 'ng', 'nl', 'nn', 'no', 'nov', 'nrm', 'nv', 'ny', 'oc',
+  'om', 'or', 'os', 'pa', 'pag', 'pam', 'pap', 'pdc', 'pi', 'pih', 'pl', 'pms',
+  'pnt', 'ps', 'pt', 'qu', 'rm', 'rmy', 'rn', 'ro', 'ru', 'rw', 'sa', 'sah',
+  'sc', 'scn', 'sco', 'sd', 'se', 'sg', 'sh', 'si', 'sk', 'sl', 'sm', 'sn',
+  'so', 'sq', 'sr', 'srn', 'ss', 'st', 'stq', 'su', 'sv', 'sw', 'szl', 'ta',
+  'te', 'tet', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tpi', 'tr', 'ts',
+  'tt', 'tum', 'tw', 'ty', 'udm', 'ug', 'uk', 'ur', 'uz', 've', 'vec', 'vi',
+  'vls', 'vo', 'wa', 'war', 'wo', 'wuu', 'xal', 'xh', 'yi', 'yo', 'za', 'zea',
+  'zh', 'zu', 
+]
 
 # Keyword for identifying category labels in different languages
 # TODO: Complete this list
