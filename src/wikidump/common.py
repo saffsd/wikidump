258059dafa02b510f7a76e779852069106b43fc0
Marco Lui <saffsd@gmail.com>
2013-01-31 13:34:21 +1100

    added a Timer class to common

--- src/wikidump/common.py
+++ src/wikidump/common.py
@@ -46,3 +46,40 @@ class Shelved(object):
       """Return the function's docstring."""
       return self.func.__doc__
 
+from timeit import default_timer
+class Timer(object):
+  def __init__(self):
+    self.timer = default_timer
+    self.start = None
+    self.end = None
+
+  def __enter__(self):
+    self.start = self.timer()
+    self.end = None
+    return self
+
+  def __exit__(self, *args):
+    self.end = self.timer()
+
+  @property
+  def elapsed(self):
+    now = self.timer()
+    if self.end is not None:
+      return self.end - self.start
+    else:
+      return now - self.start
+
+  def rate(self, count):
+    now = self.timer()
+    if self.start is None:
+      raise ValueError("Not yet started")
+
+    return count / (now - self.start)
+
+  def ETA(self, count, target):
+    """
+    Linearly estimate the ETA to reach target based on the current rate.
+    """
+    rate = self.rate(count)
+    time_left = timedelta(seconds=int((target-count) / rate))
+    return time_left 
