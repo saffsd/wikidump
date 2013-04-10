2cb46dd106270d497b1b317d376b7098a6fcb051
Marco Lui <saffsd@gmail.com>
2013-04-10 10:44:13 +1000

    rewrote cli tool to make use of argparse's subcommand functionality

--- src/wikidump/__init__.py
+++ src/wikidump/__init__.py
@@ -5,178 +5,97 @@ import logging
 import argparse 
 import tempfile
 
+from argparse import ArgumentParser
+
 logger = logging.getLogger('wikidump')
 
-from utils import build_index, load_dump, load_dumps, find_dumps
+import dataset
+from utils import build_index, load_dumps, find_dumps
 from model import Dump
 from config import config
 
-class ReTry(Exception): pass
 
-def main():
-  # TODO: make the loglevel configurable
-  logging.basicConfig(level=logging.INFO)
-  if len(sys.argv) < 2:
-    logger.error("Must specify a command! Try 'help'")
-    sys.exit(-1)
+def c_list(args):
+  """
+  List available dumps.
+  """
+  dumps = find_dumps()
+  # TODO: Print if an index exists
+  for key in sorted(dumps):
+    print "  %-20s%s" % (key, dumps[key])
 
-  command = sys.argv[1]
+def c_index(args):
+  """
+  Build index.
+  """
+  build_index()
 
-  if command == 'help':
-    # Display a help message
-    logger.info("The following commands are supported:")
-    logger.info("    index             : builds indexes - warning: can take many hours")
-    logger.info("    stats             : print statistics in a tab-delimited CSV format")
-    logger.info("    categories [lang] : list categories for 'lang', and the number of documents in each")
+def c_stats(args):
+  """
+  Output stats
+  """
+  root_logger = logging.getLogger()
+  root_logger.level = logging.ERROR
+  # Display statistics
+  paths = find_dumps()
+  sizes = dict((k, len(Dump(p))) for k, p in paths.iteritems())
 
-  elif command == 'list':
-    # List available dumps
-    dumps = find_dumps()
-    # TODO: Print if an index exists
-    for key in sorted(dumps):
-      print "  %-20s%s" % (key, dumps[key])
-    
-  elif command == 'index':
-    # Build indices
-    build_index()
+  fields = ['lang', 'filename', 'pages', 'categories']
+  outfile = csv.DictWriter(sys.stdout, fields)
+  for p in sorted(sizes, key=sizes.get, reverse=True):
+    dump = Dump(paths[p])
+    d = dict\
+          ( lang=p
+          , filename=os.path.basename(paths[p])
+          , pages=sizes[p]
+          , categories=len(dump.categories)
+          )
+    outfile.writerow(d)
 
-  elif command == 'stats':
-    root_logger = logging.getLogger()
-    root_logger.level = logging.ERROR
-    # Display statistics
-    paths = find_dumps()
-    sizes = dict((k, len(Dump(p))) for k, p in paths.iteritems())
-
-    fields = ['lang', 'filename', 'pages', 'categories']
-    outfile = csv.DictWriter(sys.stdout, fields)
-    for p in sorted(sizes, key=sizes.get, reverse=True):
-      dump = Dump(paths[p])
-      d = dict\
-            ( lang=p
-            , filename=os.path.basename(paths[p])
-            , pages=sizes[p]
-            , categories=len(dump.categories)
-            )
-      outfile.writerow(d)
-
-  elif command == 'categories':
-    # Dump category distribution
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--language", dest="lang", help="Relevant language prefix")
-    args = parser.parse_args(sys.argv[2:])
-
-    dump = load_dumps([args.lang], build_index=True)[args.lang]
+def c_categories(args):
+    dump = load_dumps([args.language], build_index=True)[args.lang]
     cats = dump.categories
 
     for c in sorted(cats, key=lambda x:len(cats[x]), reverse=True):
       print "%-4d %s" % (len(cats[c]), c)
 
-  elif command == 'sample':
-    import tarfile, subprocess, shutil, random, time, re
-    from cStringIO import StringIO
+def main():
+  parser = argparse.ArgumentParser()
 
-    import regexps
-    import dataset
-    # Sample a number of documents at random
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--language", action="append", help="Relevant language prefix")
-    parser.add_argument("-n", "--number", help="Number of files to select", type=int)
-    parser.add_argument("-o", "--output", help="Output file (will be in tbz format)", default="sample.tbz")
-    parser.add_argument("-t", "--temp", metavar='TEMPDIR', help="store temporary files in TEMPDIR")
-    parser.add_argument("--clean", action="store_true", default=False, help="apply heuristic mediawiki markup removal")
-    parser.add_argument("--minlen", help="Minimum length in bytes", type=int)
-    parser.add_argument("--langs", help="path to a file containing a list of language prefixes") 
-    parser.add_argument("--skip_index", action="store_true", default=False, help="skip indexing stage, assumes indexing has already been performed")
-    args = parser.parse_args(sys.argv[2:])
+  subparsers = parser.add_subparsers(help='available commands')
 
-    if args.language and args.langs:
-      parser.error("--langs and --language are mutually exclusive") 
+  list_subparser = subparsers.add_parser('list', help='list available dumps')
+  list_subparser.set_defaults(func=c_list)
 
-    if args.language:
-      langs = args.language
-    elif args.langs:
-      langs = map(str.strip, open(args.langs))
-    else:
-      raise ValueError("no languages to process")
+  index_subparser = subparsers.add_parser('index', help='builds indexes - (may take hours)')
+  index_subparser.set_defaults(func=c_index)
 
-    tempfile.tempdir = args.temp
+  stats_subparser = subparsers.add_parser('stats', help='print statistics in a tab-delimited CSV format')
+  stats_subparser.set_defaults(func=c_stats)
 
-    now = time.time()
-    path = os.path.join(config.get('paths','scratch'), 'sample-{0}.tar'.format(args.number))
-    build_index = not(args.skip_index)
+  categories_subparser = subparsers.add_parser('categories', help='categories info for a given language')
+  categories_subparser.set_defaults(func=c_categories)
+  categories_subparser.add_argument("language", help="Relevant language prefix")
 
-    with tarfile.open(path, 'w') as tar:
-      for lang in langs:
-        try:
-          dump = load_dump(lang, build_index=build_index, unpack=True)
-        except KeyError:
-          logger.error("do not have a dump for %s, skipping", lang)
-          continue
+  dataset_subparser = subparsers.add_parser('dataset', help='generate dataset by random sampling')
+  dataset_subparser.set_defaults(func=dataset.randomsample)
+  dataset_subparser.add_argument("-n", "--number", help="Number of files to select", type=int)
+  dataset_subparser.add_argument("-t", "--temp", metavar='TEMPDIR', help="store temporary files in TEMPDIR")
+  dataset_subparser.add_argument("--clean", action="store_true", default=False, help="apply heuristic mediawiki markup removal")
+  dataset_subparser.add_argument("--minlen", help="Minimum length in bytes", type=int)
+  dataset_subparser.add_argument("--skip_index", action="store_true", default=False, help="skip indexing stage, assumes indexing has already been performed")
 
-        chosen = set() #keeps track of ids that have been chosen
-        used = set() #keeps track of ids that have been examined
-        
-        # Adapted from wikicontent
-        while len(chosen) < args.number:
-          logger.debug("chose {0}/{1} so far".format(len(chosen), args.number))
-          try:
-            id = random.choice(xrange(dump.metadata['size']))
-            if id in used:
-              if len(used) >= dump.metadata['size']:
-                # We have run out of documents to consider. Bail out.
-                logger.warning("ran out of documents for %s", lang)
-                break
-              raise ReTry("already considered {0}".format(id))
+  group = dataset_subparser.add_mutually_exclusive_group(required=True)
+  group.add_argument("-l", "--language", action="append", help="Relevant language prefix")
+  group.add_argument("--langs", help="path to a file containing a list of language prefixes") 
 
-            used.add(id)
-            logger.debug("processing {0}".format(id))
+  dataset_subparser.add_argument("output", help="Output file (will be in tbz format)")
+  args = parser.parse_args()
 
-            page = dump.get_page_by_index(id)
+  # TODO: make the loglevel configurable
+  logging.basicConfig(level=logging.INFO)
 
-            if args.clean:
-              # apply mediawiki removal
-              text = dataset.remove_mediawiki_syntax(page.text)
-              para = dataset.paragraphs(text)
+  # invoke subcommand
+  args.func(args)
 
-              content = []
-              for p in para:
-                p = re.sub('\s',' ', p)
-                if regexps.langref.match(p): continue
-                if regexps.assoc.match(p): continue
-                if regexps.syntax.search(p): continue
-                p = regexps.tripquote.sub('\g<name>',p)
-                p = regexps.doubquote.sub('\g<name>',p)
-                u = p.decode('utf8').strip()
-                if regexps.category_name.search(u): continue
-                content.append(u)
-              if not content: raise ReTry, "No usable content"
 
-              document = '\n\n'.join(u.encode('utf8') for u in content) + '\n'
-            else:
-              # output raw mediawiki
-              document = page.text
-
-            if len(document) < args.minlen: 
-              raise ReTry, "Too short ({0}<{1})".format(len(text), args.minlen)
-            
-            logger.debug("adding {0}".format(id))
-            info = tarfile.TarInfo('{0}/{1}'.format(lang, id))
-            info.size = len(document)
-            info.mtime = now
-
-            chosen.add(id)
-            tar.addfile(info, StringIO(document))
-            logger.debug("added {0}".format(id))
-
-          except ReTry, e:
-            logger.debug("Reject: %s", e.args[0])
-            continue
-        logger.info("chose %d documents for %s", len(chosen), lang)
-        
-    # Done with the tarfile, now to bzip it
-    subprocess.call(['bzip2',path])
-    shutil.move(path+'.bz2', args.output)
-
-  else:
-    logging.error("Unknown command: %s", command)
-    logging.info("Try the 'help' command.")
--- src/wikidump/dataset.py
+++ src/wikidump/dataset.py
@@ -5,8 +5,13 @@ import re
 import os
 import shutil
 
+import logging
+logger = logging.getLogger('wikidump.config')
+
 import regexps
 import utils
+from config import config
+from utils import load_dump
 
 mediawiki_ignore_stringheads = '{}|=*#:'
 def remove_mediawiki_syntax(text):
@@ -70,4 +75,96 @@ def refresh_dir(path):
   os.mkdir(path)
   return path
 
+import tarfile, subprocess, shutil, random, time, re
+import regexps, tempfile
+from cStringIO import StringIO
 
+class ReTry(Exception): pass
+
+def randomsample(args):
+  """
+  Produce a dataset by sampling.
+  """
+
+  if args.language:
+    langs = args.language
+  elif args.langs:
+    langs = map(str.strip, open(args.langs))
+
+  tempfile.tempdir = args.temp
+
+  now = time.time()
+  path = os.path.join(config.get('paths','scratch'), 'sample-{0}.tar'.format(args.number))
+  build_index = not(args.skip_index)
+
+  with tarfile.open(path, 'w') as tar:
+    for lang in langs:
+      try:
+        dump = load_dump(lang, build_index=build_index, unpack=True)
+      except KeyError:
+        logger.error("do not have a dump for %s, skipping", lang)
+        continue
+
+      chosen = set() #keeps track of ids that have been chosen
+      used = set() #keeps track of ids that have been examined
+      
+      # Adapted from wikicontent
+      while len(chosen) < args.number:
+        logger.debug("chose {0}/{1} so far".format(len(chosen), args.number))
+        try:
+          id = random.choice(xrange(dump.metadata['size']))
+          if id in used:
+            if len(used) >= dump.metadata['size']:
+              # We have run out of documents to consider. Bail out.
+              logger.warning("ran out of documents for %s", lang)
+              break
+            raise ReTry("already considered {0}".format(id))
+
+          used.add(id)
+          logger.debug("processing {0}".format(id))
+
+          page = dump.get_page_by_index(id)
+
+          if args.clean:
+            # apply mediawiki removal
+            text = remove_mediawiki_syntax(page.text)
+            para = paragraphs(text)
+
+            content = []
+            for p in para:
+              p = re.sub('\s',' ', p)
+              if regexps.langref.match(p): continue
+              if regexps.assoc.match(p): continue
+              if regexps.syntax.search(p): continue
+              p = regexps.tripquote.sub('\g<name>',p)
+              p = regexps.doubquote.sub('\g<name>',p)
+              u = p.decode('utf8').strip()
+              if regexps.category_name.search(u): continue
+              content.append(u)
+            if not content: raise ReTry, "No usable content"
+
+            document = '\n\n'.join(u.encode('utf8') for u in content) + '\n'
+          else:
+            # output raw mediawiki
+            document = page.text
+
+          if len(document) < args.minlen: 
+            raise ReTry, "Too short ({0}<{1})".format(len(text), args.minlen)
+          
+          logger.debug("adding {0}".format(id))
+          info = tarfile.TarInfo('{0}/{1}'.format(lang, id))
+          info.size = len(document)
+          info.mtime = now
+
+          chosen.add(id)
+          tar.addfile(info, StringIO(document))
+          logger.debug("added {0}".format(id))
+
+        except ReTry, e:
+          logger.debug("Reject: %s", e.args[0])
+          continue
+      logger.info("chose %d documents for %s", len(chosen), lang)
+      
+  # Done with the tarfile, now to bzip it
+  subprocess.call(['bzip2',path])
+  shutil.move(path+'.bz2', args.output)
